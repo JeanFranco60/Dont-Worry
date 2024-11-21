60021e5d21f40f7f5c7d33ef773bbc2397fe9305
JeanFranco <jeansueiro60@gmail.com>
2024-11-20 22:04:32 -0300

    footer responsive

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,20 +1,20 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import { Link } from "react-router-dom";
-import { FaFacebookF, FaInstagram, FaTiktok} from "react-icons/fa";
+import { FaFacebookF, FaInstagram, FaTiktok } from "react-icons/fa";
 
 function Footer() {
   return (
     <footer className="bg-light py-4">
       <Container>
         <Row className="text-center text-md-start">
-          <Col md={3} className="mb-3 mb-md-0">
+          <Col xs={12} md={3} className="mb-3 mb-md-0">
             <h5 className="text-uppercase fw-bold mb-3">Sobre Nosotros</h5>
             <p className="small">
               Creamos collares únicos para el hombre moderno y sofisticado.
             </p>
           </Col>
-          <Col md={3} className="mb-3 mb-md-0">
+          <Col xs={12} md={3} className="mb-3 mb-md-0">
             <h5 className="text-uppercase fw-bold mb-3">Enlaces Rápidos</h5>
             <ul className="list-unstyled">
               <li>
@@ -39,18 +39,22 @@ function Footer() {
               </li>
             </ul>
           </Col>
-          <h5 className="text-uppercase fw-bold mb-3">Contacto</h5>
-          <Col md={3} className="mb-3 mb-md-0">
+          <Col xs={12} md={3} className="mb-3 mb-md-0">
+            <h5 className="text-uppercase fw-bold mb-3">Contacto</h5>
             <p className="small mb-0">Email: info@dontworry.com</p>
             <p className="small">Teléfono: (+598) 99-789-800</p>
           </Col>
-          <Col md={3}>
+          <Col xs={12} md={3}>
             <h5 className="text-uppercase fw-bold mb-3">Síguenos</h5>
             <div className="d-flex justify-content-center justify-content-md-start gap-3">
               <a href="#" className="text-dark" aria-label="Facebook">
                 <FaFacebookF size={24} />
               </a>
-              <a href="https://www.instagram.com/dontuworry222/" className="text-dark" aria-label="Instagram">
+              <a
+                href="https://www.instagram.com/dontuworry222/"
+                className="text-dark"
+                aria-label="Instagram"
+              >
                 <FaInstagram size={24} />
               </a>
               <a href="#" className="text-dark" aria-label="TikTok">
