e86da10adb48cf94f08db880f8581da2af62995a
JeanFranco <jeansueiro60@gmail.com>
2024-11-22 04:17:49 -0300

    duplicate file removal

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,23 +1,30 @@
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import Home from './pages/Home';
-import About from './pages/AboutUs';
-import Products from './pages/Products';
-import NotFound from './pages/NotFound';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Switch,
+} from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/AboutUs";
+import Products from "./pages/Products";
+import NotFound from "./pages/NotFound";
 import "./App.css";
 
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const App = () => {
-    return (
-        <Router>
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/about" element={<About />} />
-                <Route path="/products" element={<Products />} />
-                <Route path="*" element={<NotFound />} />
-            </Routes>
-        </Router>
-    );
+  return (
+    <Router>
+      <Switch>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Switch>
+    </Router>
+  );
 };
 
 export default App;
